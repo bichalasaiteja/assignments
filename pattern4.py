ea33aac8a82e73b7b7befe09f9c091d184d5deb4
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-14 17:55:17 -0800

    fourth commit

---
+++ pattern4.py
@@ -0,0 +1,42 @@
+#num=int(input("enter the number of rows"))
+#for i in range (num):
+#    x=65
+#    for j in range(i+1):
+#        charcter=chr(x)
+#        print(charcter,end=" ")
+#        x=x+1
+#    print()
+
+    
+    
+#num=int(input("enter the number of rows"))
+#for i in range (num):
+#    x=65
+#    for j in range(i):
+#        character=chr(x)
+#        print(character,end=" ")
+#        x=x+1
+#    print()
+
+
+
+#num=int(input("enter the number of rows"))
+#for i in range (num):
+#    x=65
+#    for j in range(i):
+#        character=chr(x)
+#        print(character,end=" ")
+#        x=x
+#    print()
+
+
+
+
+num=int(input("enter the number of rows"))
+for i in range (num):
+    x=65
+    for j in range(i+1):
+        character=chr(x)
+        print(character,end=" ")
+        x=x+1
+    print()
