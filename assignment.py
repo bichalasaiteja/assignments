49c786c1911eb3bdc2966f29650deb3aea6f0db1
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-14 17:58:18 -0800

    assignment

---
+++ assignment.py
@@ -0,0 +1,10 @@
+num1=float(input("enter the first number"))
+num2=float(input("enter the second number"))
+
+
+if num1>num2:
+    print(num1,"is the biggest number")
+elif num2>num1:
+    print(num2,"is the biggest number")
+else:
+    print("both numbers are equal")
