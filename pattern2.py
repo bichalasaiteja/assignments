36c6772d320cc4b6f85d7a954f73215e32dff54e
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-14 17:50:22 -0800

    second pattern

---
+++ pattern2.py
@@ -0,0 +1,57 @@
+#num=int(input("enter the number of rows"))
+#n=65
+#for i in range(num):
+#    for j in range(i+1):
+#        charcter=chr(n)
+#        print(charcter,end=" ")
+#    n=n+1
+#    print()
+
+
+
+
+#num=int(input("enter the number of rows"))
+#n=65
+#for i in range(num):
+#    for j in range(i+1):
+#        charcter=chr(n)
+#        print(charcter,end=" ")
+#    n=n+2
+#    print()
+
+
+
+
+#num=int(input("enter the number of rows"))
+#n=65
+#for i in range(num):
+#    for j in range(i):
+#        charcter=chr(n)
+#        print(charcter,end=" ")
+#    n=n+1
+#    print()
+
+
+
+
+
+#num=int(input("enter the number of rows"))
+#n=65
+#for i in range(num):
+#    for j in range(i+10):
+#        charcter=chr(n)
+#        print(charcter,end=" ")
+#    n=n+1
+#    print()
+
+
+
+
+num=int(input("enter the number of rows"))
+n=65
+for i in range(num):
+    for j in range(1+i):
+        charcter=chr(n)
+        print(charcter,end=" ")
+    n=n+1
+    print()
