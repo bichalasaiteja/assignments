8e344707351da62f8c41cfe76fa1c5bf6a9fde07
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-15 12:59:20 -0800

    upperandlower

---
+++ upperandlower.py
@@ -0,0 +1,42 @@
+#upper
+
+#a="python is a programing language"
+#b="it is good subject"
+#print(a.upper())
+#print(b.upper())
+
+
+#lower
+
+#a="PYTHON"
+#b="iT IS GOOD SUBJECT"
+#print(a.lower())
+#print(b.lower())
+
+
+#title
+
+#a="python is good"
+#b="cooding"
+#print(a.title())
+#print(b.title())
+
+
+#a="this is a book"
+#x=a.title()
+#print(x)
+
+
+#swap
+
+#a="i am a python developper"
+#s=a.swapcase()
+#print(s)
+
+
+#capital first letter
+
+
+a="i am a good boy"
+s=a.capitalize()
+print(s)
