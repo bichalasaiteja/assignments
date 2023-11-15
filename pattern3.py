868b0829ae2b37e614a6a1153980ff300e3b1ea0
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-14 17:53:17 -0800

    third commit

---
+++ pattern3.py
@@ -0,0 +1,57 @@
+#num=int(input("enter the number of rows"))
+#n=65
+#for i in range(num):
+#    for j in range(i+1):
+#        if i%2==0:
+#            print('A',end=" ")
+#        else:
+#            print('B',end=' ')    
+#    print()
+
+
+
+#num=int(input("enter the number of coloums"))
+#n=65
+#for i in range(num):
+#    for j in range(i+1):
+#        charcter=chr(n)
+#        print(charcter,end=" ")
+#    n=n
+#    print()
+
+
+
+#num=int(input("enter the number of rows"))
+#n=65
+#for i in range(num):
+#    for j in range(i+1):
+#        if i%2==1:
+#            print('A',end=" ")
+#        else:
+#            print('B',end=' ')    
+#    print()
+
+
+
+#num=int(input("enter the number of rows"))
+#n=65
+#for i in range(num):
+#    for j in range(i+1):
+#        if i%2==0:
+#            print('A')
+#        else:
+#            print('B')    
+#    print()
+
+
+
+
+num=int(input("enter the number of rows"))
+n=65
+for i in range(num):
+    for j in range(i):
+        if i%2==1:
+            print('A',end=" ")
+        else:
+            print('B',end=' ')    
+    print()
