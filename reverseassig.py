9d85360a9f9bfe17e514fc7b96e1501938102f32
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-15 12:16:03 -0800

    reverse commit

---
+++ reverseassig.py
@@ -0,0 +1,35 @@
+#slicing
+
+name=input("enter your string: ")
+print("original string is: ",name)
+name=name[::-1]
+print("reversed string is: ",name)
+
+
+#whileloop
+
+
+#name=input("enter a string: ")
+#print("original string is: ",name)
+#s_name=""
+#count=len(name)
+#while count>0:
+#    s_name=s_name+name[count-1]
+#    count=count-1
+#print("reversed string is: ",s_name)
+
+
+#forloop
+
+
+
+#name=input("enter a string: ")
+#print("original string is: ",name)
+#s_name=""
+#for char in name:
+#    s_name=char+s_name
+#print("reverse string is: ",s_name)
+
+
+
+
