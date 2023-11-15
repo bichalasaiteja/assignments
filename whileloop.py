30894626e260b3a6185fb6a06826bb9a12a1ce70
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-14 18:07:12 -0800

    whilwloop

---
+++ whileloop.py
@@ -0,0 +1,30 @@
+#ussername=""
+#password=""
+#while ussername!="sai":
+#    ussername=("enter your ussername to verify : ")
+    #password=("enter your password to verify : ")
+
+#print("hello",ussername,"thank you for verification")
+#while password!="saiteja":
+    #password=("enter your password to verify : ")
+#print("hello",password,"thank you for your verification")
+
+
+
+
+#ussername=""
+#password=""
+#while ussername!="saiteja":
+#    ussername=input("enter the ussername to verify:")
+#while password==1234567:
+#    password!=int(input("enter the password to verify:"))
+#print("thanks for verification")    
+
+
+
+ussername=""
+password=""
+while (ussername!="saiteja")or(password!=12345678):
+    ussername=input("enter the ussername to verify:")
+    password!=input("enter the password to verify:")
+print("thanks for verification")
