ef37381dea83b358961441dff30ce73685af67ce
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2023-11-14 17:10:33 -0800

    first commit

---
+++ pattern1 09-11-23.py
@@ -0,0 +1,61 @@
+#for i in range(4):
+#    for j in range(4):
+#        print("*",end="")
+
+
+
+#for i in range(4):
+#    for j in range(4):
+#        print("*",end=" ")
+#    print()
+
+
+#for i in range(4):
+#    for j in range(4):
+#        print("*")
+#    print()
+
+
+#n=int(input("enter the number : "))
+#for i in range(1,n+1):
+#    for j in range(1,n+2-i):
+#        print("*",end=" ")
+#    print()
+
+
+
+#n=int(input("enter the number : "))
+#for i in range(1,n+1):
+#    for j in range(i):
+#       print("*",end=" ")
+#    print()
+
+
+
+#n=int(input("enter the number : "))
+#for i in range(1,n+1):
+#    for j in range(i):
+#        print("*",end=" ")
+#    print()
+#for i in range(1,n+1):
+#    for j in range(1,n+2-i):
+#        print("*",end=" ")
+#    print()
+
+
+
+#n=int(input("enter the number : "))
+#for i in range(1,n+1):
+#    for j in range(i+1):
+#        print("*",end=" ")
+#    print()
+
+
+
+n=int(input("enter the number : "))
+for i in range(1,n+1):
+    for j in range(1,n+2-i):
+        print("*",end=" ")
+    print()
+
+
