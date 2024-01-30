9b37df1a2416c8f979d628ef8a4612c6fd605175
Bichala Saiteja <bichalasaiteja.marolix@gmail.com>
2024-01-30 15:57:47 -0800

    instance

---
+++ instance variable.py
@@ -0,0 +1,35 @@
+class Vehicle:
+    pass
+  
+Vehicle1=Vehicle()
+
+#class Student:
+#    school = "freeCodeCamp.org"
+    
+#    def __init__(self, name, course):
+#        self.name = name
+#        self.course = course
+    
+#Student1 = Student("sai", "JavaScript")
+#Student2 = Student("saiteja", "Python")
+
+#print(Student1.name) 
+#print(Student2.name) 
+
+
+#class Vehicle:
+#    def __init__(self, name, max_speed, mileage):
+#        self.name = name
+#        self.max_speed = max_speed
+#        self.mileage = mileage
+
+#class Bus(Vehicle):
+#    pass
+
+#School_bus = Bus("School Volvo", 180, 12)
+#print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
+
+
+
+
+
